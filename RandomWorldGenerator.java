82ae385b14f07368846fa0574e7f75f5fd77426c
fulanito91 <ismaeljimenezramos@gmail.com>
2024-05-21 13:24:51 +0200

    creación del archivo RandomWorldGenerator.java

---
+++ RandomWorldGenerator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.randomworldgenerator;
+
+/**
+ *
+ * @author ismae
+ */
+public class RandomWorldGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
+
