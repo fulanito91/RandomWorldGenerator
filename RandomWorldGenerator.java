832d5a390a5ef69fb2d3303f02275eb9a02b9f2f
fulanito91 <ismaeljimenezramos@gmail.com>
2024-05-22 08:40:09 +0200

    Update RandomWorldGenerator.java
    adios mundo

--- RandomWorldGenerator.java
+++ RandomWorldGenerator.java
@@ -12,6 +12,7 @@ public class RandomWorldGenerator {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Adios Mundo");
     }
 }
 
